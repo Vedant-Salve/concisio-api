9f015622991f5c3a5cb8bd144d3beeadc029051b
Vedant Salve <vedantsalve13@gmail.com>
2025-04-11 16:17:05 +0530

    text_summary.py

--- text_summary.py
+++ text_summary.py
@@ -3,76 +3,36 @@ from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
 from heapq import nlargest
 
-text='''Lelouch vi Britannia is an exiled Britannian prince, the son of Emperor Charles zi Britannia and his royal consort Marianne vi Britannia.
- Lelouch has a little sister, Nunnally Vi Britannia. Marianne was brutally murdered in the palace, and Nunnally, who witnessed the murder of their mother, was so traumatized that she lost her sight, and stray bullets to her lower body took away her ability to walk.
-   Lelouch is furious with his father, believing he failed his mother and sister by turning a blind eye to their mother's death and failing to pursue their mother's killer.'''
+# Load model once globally
+nlp = spacy.load("en_core_web_sm")
 
 def summarizer(rawdoc):
-    # list of stopword
-    stopwords=list(STOP_WORDS)
-    # print(stopwords)
+    stopwords = list(STOP_WORDS)
+    doc = nlp(rawdoc)
 
-    nlp = spacy.load("en_core_web_sm")
-    doc=nlp(rawdoc)
-    # print(doc)
-
-    # tokenization on text
-    tokens=[token.text for token in doc]
-    # print(tokens)
-
-    # getting word and its frequency
-    word_freq={}
+    # Word frequency (excluding stopwords and punctuation)
+    word_freq = {}
     for word in doc:
-        if word.text.lower() not in stopwords and word.text.lower() not in punctuation:
-            if word.text not in word_freq.keys():
-                word_freq[word.text]=1
-            else:
-                word_freq[word.text]+=1
+        word_text = word.text.lower()
+        if word_text not in stopwords and word_text not in punctuation:
+            word_freq[word_text] = word_freq.get(word_text, 0) + 1
 
-    # print(word_freq)
+    # Normalize frequencies
+    max_freq = max(word_freq.values(), default=1)
+    word_freq = {word: freq / max_freq for word, freq in word_freq.items()}
 
-    # getting count of word with maximum frequency
-    max_freq=max(word_freq.values())
-    # print(max_freq)
-
-    # normalizing frequencies of word by dividing every word frequency by max frequency
-    for word in word_freq.keys():
-        word_freq[word]=word_freq[word]/max_freq
-
-    # print(word_freq)
-
-    # sentence tokenization
-    sent_tokens=[sent for sent in doc.sents]
-    # print(sent_tokens)
-
-    # getting sentence and summing its word frequencies from word_freq
-    sent_scores={}
+    # Sentence scoring
+    sent_tokens = list(doc.sents)
+    sent_scores = {}
     for sent in sent_tokens:
         for word in sent:
-            if word.text in word_freq.keys():
-                if sent not in sent_scores.keys():
-                    sent_scores[sent]=word_freq[word.text]
-                else:
-                    sent_scores[sent]+=word_freq[word.text]
-    # print(sent_scores)
+            if word.text.lower() in word_freq:
+                sent_scores[sent] = sent_scores.get(sent, 0) + word_freq[word.text.lower()]
 
-    # how many sentence you want in your summary
-    select_len=int(len(sent_tokens)*0.5)
-    # print(select_len)
+    # Top n sentences
+    select_len = max(1, int(len(sent_tokens) * 0.5))
+    summary = nlargest(select_len, sent_scores, key=sent_scores.get)
+    sorted_summary = sorted(summary, key=lambda sent: sent.start)
 
-    # get the sentences with highest frequencies
-    summary=nlargest(select_len , sent_scores , key=sent_scores.get)
-    # print(summary)
-
-    # Sort them in the original order as they appear in the text
-    sorted_summary=sorted(summary,key=lambda sent: sent.start)
-
-    # converting summary form list to text
-    final_summary=[word.text for word in sorted_summary]
-    summary=" ".join(final_summary)
-    # print(summary)
-
-    # print("Text: ",text)
-    # print("Summary: ",summary)
-
-    return summary,doc,len(rawdoc.split(" ")),len(summary.split(" "))
+    final_summary = " ".join([sent.text for sent in sorted_summary])
+    return final_summary, doc, len(rawdoc.split()), len(final_summary.split())
